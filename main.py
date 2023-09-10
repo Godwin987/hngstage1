3d62e56d305828c2d823528429f06496b50b7a15
Godwin987 <basseygodwin911@gmail.com>
2023-09-10 22:58:16 +0100

    Response modification

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from datetime import datetime
 
-
 app = Flask(__name__)
-response = {
-    'current_day': f'{datetime.day}',
-    'utc_time': f'{datetime.time()}',
-    'github_file_url':
-}
-@app.route('/get/<slack_name>')
+
+
+@app.route('/api/')
 def index():
-    return "hello"
-
-
+    response = {
+        'slack_name': request.args.get('slack_name'),
+        'current_day': f'{datetime.now().strftime("%A")}',
+        'utc_time': f'{datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")}',
+        'track': request.args.get('track'),
+        'github_file_url': 'https://github.com/Godwin987/hngstage1/blob/master/main.py',
+        'github_repo_url': 'https://github.com/Godwin987/hngstage1',
+        'status_code': 200
+    }
+    response = jsonify(response)
+    return response
 
 
 if __name__ == '__main__':
